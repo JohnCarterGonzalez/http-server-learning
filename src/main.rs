120db7c77fbe2523f74908527adfb35970df74b6
johncartergonzalez <unmrf0ngh@mozmail.com>
2023-09-30 08:45:04 -0700

    bound server to a port, responded with 200 OK

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io::Write;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:4200").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n");
+                println!("new connection accepted");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
