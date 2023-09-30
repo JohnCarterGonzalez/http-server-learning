89cfacc980244110ac2071d05851414b5788b412
johncartergonzalez <unmrf0ngh@mozmail.com>
2023-09-30 08:46:24 -0700

    fix: added leading b in stream.write_all()

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n");
+                let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
                 println!("new connection accepted");
             }
             Err(e) => {
